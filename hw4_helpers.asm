47d858dc51e09ee55242c6451f0683807e134643
powcoder代写 <powcoder@163.com>
2022-07-17 12:57:17 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ hw4_helpers.asm
@@ -0,0 +1,42 @@
+.text
+
+validKingMove:
+
+	# COMMNET/UNCOMMENT THE LINE FOR THE CASE YOU WANT TO TEST
+
+	li $v0, -2  #for error with input arguments (invalid values for from, to, player)
+	#li $v0, -1  # if the move is invalid
+	#li $v0, 0   # if the move is valid and unobstructed.
+	#li $v0, 1   #if the move is valid and a piece is captured.
+
+	jr $ra
+
+validQueenMove:
+	# COMMNET/UNCOMMENT THE LINE FOR THE CASE YOU WANT TO TEST
+
+	li $v0, -2  #for error with input arguments (invalid values for from, to, player)
+	#li $v0, -1  # if the move is invalid
+	#li $v0, 0   # if the move is valid and unobstructed.
+	#li $v0, 1   #if the move is valid and a piece is captured.
+
+	jr $ra
+
+validKnightMove:
+	# COMMNET/UNCOMMENT THE LINE FOR THE CASE YOU WANT TO TEST
+
+	li $v0, -2  #for error with input arguments (invalid values for from, to, player)
+	#li $v0, -1  # if the move is invalid
+	#li $v0, 0   # if the move is valid and unobstructed.
+	#li $v0, 1   #if the move is valid and a piece is captured.
+
+	jr $ra
+
+validPawnMove:
+	# COMMNET/UNCOMMENT THE LINE FOR THE CASE YOU WANT TO TEST
+
+	li $v0, -2  #for error with input arguments (invalid values for from, to, player)
+	#li $v0, -1  # if the move is invalid
+	#li $v0, 0   # if the move is valid and unobstructed.
+	#li $v0, 1   #if the move is valid and a piece is captured.
+
+	jr $ra
